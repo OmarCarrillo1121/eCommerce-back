49ad24d4191a28d89a0e52a993d74ae8993e5cd8
patorodriguez12 <pato_rodriguez12@outlook.com>
2023-11-08 21:34:42 -0300

    users

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db");
 //⭐const {} = require ("");
 
 //Syncing all the models at once:
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
     server.listen(3001, () => {
       //ver si agregamos algo aqui:⭐
       console.log('%s listening at 3001'); // eslint-disable-line no-console
