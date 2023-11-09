2fd37941c1171bbd5e52a2616bbb29b47cf4ed45
patorodriguez12 <pato_rodriguez12@outlook.com>
2023-11-09 18:43:55 -0300

    models routes update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const server = require ("./src/server");
 const { conn } = require("./src/db");
+const PORT = process.env.PORT || 3001;
 
 //⭐const {} = require ("");
 
 //Syncing all the models at once:
 conn.sync({ alter: true }).then(() => {
-    server.listen(3001, () => {
+    server.listen(PORT, () => {
       //ver si agregamos algo aqui:⭐
       console.log('%s listening at 3001'); // eslint-disable-line no-console
     });
