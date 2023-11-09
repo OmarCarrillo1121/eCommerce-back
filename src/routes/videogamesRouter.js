dbf8c853336ba307e28b4bd372c8069e18f8211d
Luli-espindola <lau_espindola96@hotmail.com>
2023-11-09 18:15:00 -0300

    Rutas actualizadas

--- src/routes/videogamesRouter.js
+++ src/routes/videogamesRouter.js
@@ -19,6 +19,8 @@ const editProductHandler =require("../handlers/Videogames/editProductHandler");
 /////////////////////////////////////////////////////////
 
 const videogamesRouter = Router();
+
+
 //delete:
 videogamesRouter.get("/", getDeleteFalseHandler);//False
 
