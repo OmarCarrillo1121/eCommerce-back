2cf2c06ec75a08ffe175a5bc6d14d834149f7e27
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-11-15 08:39:59 -0600

    "Small bug fix on videogames model" "Small bug fix on videogames model"

--- src/models/videogames.js
+++ src/models/videogames.js
@@ -1,64 +1,67 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('Videogames',{
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-    
-          image: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-    
-          genre: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-    
-          developer: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-    
-          platform: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-    
-          price: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-          },
+  sequelize.define(
+    "Videogames",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-          discount: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-          },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-          stock: {//⭐revisar bien si esto despues hay que sacarlo!
-            type: DataTypes.INTEGER,
-            allowNull: false,
-          },
-    
-          deleted: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-          }
+      genre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      developer: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      platform: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+
+      discount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+
+      stock: {
+        //⭐revisar bien si esto despues hay que sacarlo!
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+
+      deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
     {
-        freezeTableName: true,
-        timestamps: false,
-    })
-    
-}
+      freezeTableName: true,
+      timestamps: false,
+    }
+  );
+};
