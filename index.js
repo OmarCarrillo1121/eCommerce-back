60c9fa70fe66decc4370b62bfe258020c1117445
unknown <luis901810@gmail.com>
2023-11-18 01:24:13 -0500

    subo backen

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const server = require("./src/server");
 const { conn } = require("./src/db");
-
+const port = process.env.PORT || 3001;
 //⭐const {} = require ("");
 
 //Syncing all the models at once:
-conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
+conn.sync({ alter: true }).then(() => { 
+
+  server.listen(port, () => {
+    
     //ver si agregamos algo aqui:⭐
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log( `puerto escuchando en http://localhost:${port}`); // eslint-disable-line no-console
   });
 });
