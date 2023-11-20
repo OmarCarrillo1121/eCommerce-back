ce9f1451236a43bbf998e62a9faa41f4254fd09e
patorodriguez12 <pato_rodriguez12@outlook.com>
2023-11-20 18:49:03 -0300

    Get orders by user id added

--- src/routes/ordersRouter.js
+++ src/routes/ordersRouter.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 // importamos handler
 const getOrdersHandler = require("..//handlers/Orders/getOrdersHandler");
 const createOrderHandler = require("../handlers/Orders/createOrderHandler");
+const getOrdersByUserIdHandler = require("../handlers/Orders/getOrdersByUserIdHandler");
 const getOrderByIdHandler = require("../handlers/Orders/getOrderByIdHandler");
 const editOrderHandler = require("../handlers/Orders/editOrderHandler");
 const cancelOrderHandler = require("../handlers/Orders/cancelOrderHandler");
@@ -13,6 +14,7 @@ const ordersRouter = Router();
 // definimos rutas
 ordersRouter.get("/", getOrdersHandler);
 ordersRouter.get("/:orderId", getOrderByIdHandler);
+ordersRouter.get("/user/:userId", getOrdersByUserIdHandler);
 ordersRouter.post("/", createOrderHandler);
 ordersRouter.put("/:orderId", editOrderHandler);
 ordersRouter.delete("/cancel/:orderId", cancelOrderHandler);
