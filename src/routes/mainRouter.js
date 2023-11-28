1db5d7f02d29984788e303c8d6ef4a7e168b6e8b
Edward V <ervasalloe@unal.edu.co>
2023-11-28 11:09:45 -0500

    BackPasarela

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+//const dotenv = require("dotenv"); //Para usar variables de entorno
 //Agregar rutas:⭐
 //const  = require("./");
 const usersRouter = require("./usersRouter");
@@ -9,6 +9,7 @@ const videogamesRouter = require("./videogamesRouter");
 const bannersRouter = require("./bannersRouter");
 const reviewsRouter = require("./reviewsRouter");
 const ordersRouter = require("./ordersRouter");
+const mercadoPago = require("./mercadoPago");
 
 
 const mainRouter = Router();
@@ -28,4 +29,5 @@ mainRouter.use("/videogames", videogamesRouter);
 mainRouter.use("/banners", bannersRouter);
 mainRouter.use("/reviews", reviewsRouter);
 mainRouter.use("/orders", ordersRouter);
+mainRouter.use("/MercadoPago", mercadoPago);
 module.exports = mainRouter;
